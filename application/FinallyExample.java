78ec0cb1aeb5e52f44724966e532aaf176a47bd9
Lucas Silva <lucas.coder@bol.com.br>
2019-09-12 22:54:28 -0300

    finally block

---
+++ application/FinallyExample.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FinallyExample {
+
+	public static void main(String[] args) {
+		
+		File file = new File("/home/lucas/Documentos/teste.txt");
+		
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}catch (FileNotFoundException e) {
+			System.out.println("Error opening file: "+ e.getMessage());
+		}finally {
+			if(sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed! ");
+		}
+	}
+}
