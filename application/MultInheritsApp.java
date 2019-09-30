6e19a7e037f7c7acc12671555648f1d3a876c2ec
Lucas Silva <lucas.coder@bol.com.br>
2019-09-30 18:50:16 -0300

    diamond problem example

---
+++ application/MultInheritsApp.java
@@ -0,0 +1,27 @@
+package application;
+
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
+
+public class MultInheritsApp {
+	// this program doesn't actually do multiple inheritance, just to study the diamond problem
+	public static void main(String[] args) {
+		
+		ConcreteScanner s = new ConcreteScanner("8080");
+		s.processDoc("My email");
+		System.out.println("Scan result: "+s.scan());
+		
+		System.out.println();
+		ConcretePrinter p = new ConcretePrinter("1080");
+		p.processDoc("My letter");
+		p.print("My letter");
+		
+		System.out.println();
+		ComboDevice c = new ComboDevice("5050");
+		c.processDoc("My dissertation");
+		c.print("My dissertation");
+		System.out.println("Combo result: "+c.scan());
+		
+	}
+}
