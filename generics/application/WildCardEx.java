9b3a662d5a9dab1f76c68c7771542cc1bcb513d5
Lucas Silva <lucas.coder@bol.com.br>
2019-10-06 17:35:38 -0300

    Wildcard type example

---
+++ generics/application/WildCardEx.java
@@ -0,0 +1,21 @@
+package generics.application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WildCardEx {
+
+	public static void main(String[] args) {
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+		
+		List<String> myStrs = Arrays.asList("Lucas","Noeli","Lilian");
+		printList(myStrs);
+	}
+	
+	public static void printList(List<?> list) {
+		for(Object obj : list) {
+			System.out.println(obj);
+		}
+	}
+}
