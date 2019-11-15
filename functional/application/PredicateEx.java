4bf8d91d0c5301a70cddc5f75d029e8607581f5f
Lucas Silva <lucas.coder@bol.com.br>
2019-11-15 19:58:22 -0300

    Functional Programming - Predicate - declared lambda expression

--- functional/application/PredicateEx.java
+++ functional/application/PredicateEx.java
@@ -3,6 +3,7 @@ package functional.application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import functional.entities.Product;
 
@@ -18,7 +19,11 @@ public class PredicateEx {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::nonStaticProductPredicate);
+		Double min = 100.0;
+		
+		Predicate<Product> pred = p -> p.getPrice() >= min;
+		
+		list.removeIf(pred);
 		
 		for(Product p : list) {
 			System.out.println(p);
