7beacf0d5ed2ab09093300faeeea470c7daeacec
Lucas Silva <lucas.coder@bol.com.br>
2019-11-15 19:59:35 -0300

    Functional Programming - Predicate - inline lambda expression

--- functional/application/PredicateEx.java
+++ functional/application/PredicateEx.java
@@ -21,9 +21,7 @@ public class PredicateEx {
 		
 		Double min = 100.0;
 		
-		Predicate<Product> pred = p -> p.getPrice() >= min;
-		
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= min);
 		
 		for(Product p : list) {
 			System.out.println(p);
