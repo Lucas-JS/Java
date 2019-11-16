9846613a5340be8f3220df6960b1c47abcd15083
Lucas Silva <lucas.coder@bol.com.br>
2019-11-16 01:26:01 -0300

    Functional Programming - Consumer - interface implementation

--- functional/application/ConsumerEx.java
+++ functional/application/ConsumerEx.java
@@ -5,25 +5,22 @@ import java.util.List;
 import java.util.Locale;
 
 import functional.entities.Product;
+import functional.util.PriceUpdate;
 
-public class PredicateEx {
+public class ConsumerEx {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
-		Double min = 100.0;
-		
-		list.removeIf(p -> p.getPrice() >= min);
-		
-		for(Product p : list) {
-			System.out.println(p);
-		}
+
+		list.forEach(new PriceUpdate());
+
+		list.forEach(System.out::println);
 	}
 }
