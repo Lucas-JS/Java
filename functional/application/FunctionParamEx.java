c14c27a7b2fb8b18495e5560d8b698cbe54f6775
Lucas Silva <lucas.coder@bol.com.br>
2019-11-16 03:03:30 -0300

    Functional Programming - Function - function receiving lambda function as a parameter

---
+++ functional/application/FunctionParamEx.java
@@ -0,0 +1,28 @@
+package functional.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import functional.entities.Product;
+import functional.service.ProductService;
+
+public class FunctionParamEx {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		ProductService ps = new ProductService();
+		
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
+	}
+}
